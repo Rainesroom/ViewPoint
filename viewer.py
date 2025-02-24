bd42420ce2138f3cc4dac7ab3e86c1dee4fa1311
T_cat9000_2 <terpstragraham@gmail.com>
2025-02-23 18:46:32 -0800

    Deafualt Image Loading

--- viewer.py
+++ viewer.py
@@ -1,3 +1,15 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
 root = tk.Tk()
+root.geometry("800x600")
 root.title("ViewPoint - Picture Viewing Software. Open Source. Free For all.")
+Default_Image = Image.open('Default_ViewPoint_Image.jpg')
+Default_Image = Default_Image.resize((800, 600), Image.Resampling.LANCZOS)
+
+Default = ImageTk.PhotoImage(Default_Image)
+
+label = tk.Label(root, image=Default)
+label.pack(fill="both", expand=True)
+
+root.mainloop()
